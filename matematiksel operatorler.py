529b8ed77eda2a682c262d2bf4874e58badb4208
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-10-08 11:55:43 +0300

    matematiksel öperatörler

---
+++ matematiksel operatorler.py
@@ -0,0 +1,32 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#toplama
+print(1134+5678)
+
+#çıkarma
+print(121312-12412)
+
+#çarpma
+print(123*123)
+
+#bölme
+print(12/4)
+
+#üs
+print(2**63*10)
+
+#mod
+print(40%3)
+# mod alma = örnek = 40%3 40 ı 3 e böl kalanı yaz.
+
+#tam bölme
+print(40//3)
+exit("tamamlandı")
