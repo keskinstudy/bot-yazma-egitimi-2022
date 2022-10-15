668d5c9d0e15bf1cca7c140e7cb3cb121dd70595
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-10-15 11:30:04 +0300

    karakter dizileri

---
+++ karakter_dizileri.py
@@ -0,0 +1,36 @@
+# kullanıcı isim ve yaş değişkenlerini oluşturalım
+age = 14
+name = "ismail hakkı"
+ # kullanıcı bilgilerinizi ekrana yazdıralım
+print("adınız:" + name + ", yaşınız: " + str(age))
+
+# karakter verisinin uzunluğuna bakalım
+print(len(name))
+
+# string veri tipindeki değerin çeşitli elamanlarını ekrana yazdıralım
+# elamanlar 0 dan başlar
+
+print(name[0]) # ismin 1. harfini yazdırır
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
