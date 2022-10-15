726841f4e88b5453647ff6f28de389941c372e9c
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-10-15 12:06:17 +0300

    string formatlama

---
+++ string_formatlama.py
@@ -0,0 +1,19 @@
+# kullanıcıdan isim,soyad ve yaş bilgilerini alalım
+isim = input("isminiz:")
+soyad = input("soyadınız:")
+yas = input("yaşınız:")
+
+print("Adınız: " + isim + "" + soyad + ", yaşınız:"+yas) # uzun yöntem
+print(f"adınız {isim} {soyad} yaşınız {yas}") # kolay yöntem
+
+
+
+
+
+
+
+
+
+
+
+
