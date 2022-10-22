aeb4db11f403a985c24ea7d66a465963fc0ee733
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-10-22 11:22:22 +0300

    string kaçş dizeleri

---
+++ string_kacıs_dizileri.py
@@ -0,0 +1,33 @@
+print("ahmet \" bugün sinemaya gidiyorum.\" dedi.")
+print("yarın Adana\'ya gidiyorum.")
+# özel karakterler
+print("ismail\nhakkı") # alt satıra yazar kendisinden sonra gelen ifadeyi
+print("ismail\thakkı") # tab (hizalama)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
