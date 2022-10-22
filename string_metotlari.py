a1781a8a65f382daedb145ce3ec40fd0268a230a
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-10-22 09:59:48 +0300

    string metotları

---
+++ string_metotlari.py
@@ -0,0 +1,49 @@
+okul = "Sancaktepe Teknoloji Anadolu İmam Hatip Lisesi"
+# tüm karakterleri büyük harf yapalım
+print((okul.upper()))
+# tüm harfler küçük
+print(okul.lower())
+# kelimenin ilk harfi büyük
+print(okul.title())
+# karakter dizisinin ilk karakterini büyük diğer karakterlerini küçük yapalım
+print(okul.capitalize())
+# belirli bir ifadenin kaç defa yer aldığını bulalım ilk önce tüm harfleri küçültelim ki büyük küçük duyarlılığına takılmayalım
+print(okul.lower().count("e"))
+# soldaki ve sağdaki boşluk karakterlerini temizleyelim
+name = input("adınız:")
+print(name.strip())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
