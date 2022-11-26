8a4531f8984713265b209a84f4a9d8c95b7ee273
İsmail Hakkı KESKJİN <keskinismailhakki71@gmail.com>
2022-11-26 10:50:53 +0300

    referans tipi

---
+++ referans.py
@@ -0,0 +1,54 @@
+
+# VALUE TİPİ
+###########################################
+# iki value veri tipi oluşturalım
+a = 1
+b = 2
+
+# ikinci değişkeni ilk değişkene atayalım
+a = b
+
+# ikinci değişkenin değerini değiştirelim. birinci değişken değişti mi bakalım.
+b = 3
+print(a, b)
+
+
+# REFERANS TİPİ
+###########################################
+# iki tane referan tipi değişken oluşturalım
+x = [1, 2, 3]
+y = ["a", "b" ,"c"]
+
+# ikinci değişkeni ilk değişkene atayalım
+x = y
+
+# ikinci değişken değişiklik yaptığımızda ilk değişken etkileniyor mu bakalım.
+y[0] = "d"
+print(x, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
